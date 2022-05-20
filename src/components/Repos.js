42e70db873a0b97296403c8ffee053763a5e7449
livos <lvivancos2nd-dev@yahoo.fr>
2022-05-20 17:00:53 +0700

    fusion charts API

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,8 +4,29 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts"; // import everything in ./Charts/index.js by default
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  console.log(repos);
-  return <h2>repos component</h2>;
+
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "CSS",
+      value: "23",
+    },
+    {
+      label: "Javascript",
+      value: "80",
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <ExampleChart data={chartData} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
