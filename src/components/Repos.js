052ecd0522cdcefdd8ef30b7614df807c1254d85
livos <lvivancos2nd-dev@yahoo.fr>
2022-05-20 16:30:28 +0700

    repos setup

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts"; // import everything in ./Charts/index.js by default
 const Repos = () => {
+  const { repos } = React.useContext(GithubContext);
+  console.log(repos);
   return <h2>repos component</h2>;
 };
 
