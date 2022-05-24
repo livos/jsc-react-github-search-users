c268237657c179731f6bef4163646e19210c3121
livos <lvivancos2nd-dev@yahoo.fr>
2022-05-24 19:43:32 +0700

    load all data together after one single login Promise.allSettled()

--- src/context/context.js
+++ src/context/context.js
@@ -26,14 +26,20 @@ const GithubProvider = ({ children }) => {
     if (response) {
       setGithubUser(response.data);
       const { login, followers_url } = response.data;
-      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) =>
-        setRepos(response.data)
-      );
-      axios(`${followers_url}?per_page=100`).then((response) =>
-        setFollowers(response.data)
-      );
-      /* - [Repos](https://api.github.com/users/john-smilga/repos?per_page=100)
-         - [Followers](https://api.github.com/users/john-smilga/followers)*/
+
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ]).then((results) => {
+        const [repos, followers] = results;
+        const status = "fulfilled";
+        if (repos.status === status) {
+          setRepos(repos.value.data);
+        }
+        if (repos.status === status) {
+          setFollowers(followers.value.data);
+        }
+      });
     } else {
       toggleError(true, "there is no user with that user name");
     }
