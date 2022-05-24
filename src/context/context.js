6207119d3659f3e56d983dac46dd2b3f3dff686a
livos <lvivancos2nd-dev@yahoo.fr>
2022-05-24 19:28:08 +0700

    repos and followers

--- src/context/context.js
+++ src/context/context.js
@@ -10,7 +10,7 @@ const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
-  const [repos, setrepos] = useState(mockRepos);
+  const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
   const [requests, setRequests] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
@@ -25,6 +25,15 @@ const GithubProvider = ({ children }) => {
 
     if (response) {
       setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) =>
+        setRepos(response.data)
+      );
+      axios(`${followers_url}?per_page=100`).then((response) =>
+        setFollowers(response.data)
+      );
+      /* - [Repos](https://api.github.com/users/john-smilga/repos?per_page=100)
+         - [Followers](https://api.github.com/users/john-smilga/followers)*/
     } else {
       toggleError(true, "there is no user with that user name");
     }
